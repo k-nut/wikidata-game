0fdba9e97855dadc9b8fe2c8e9aa3b0dcdf48b48
Knut Hühne <knut@k-nut.eu>
2015-05-17 19:07:17 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+class Minister(object):
+    def __init__(self, wikidata_id, photo_url):
+        self.wikidata_id = wikidata_id
+        self.photo_url = photo_url
+        self.name = "<unset>"
+
+    def get_label(self):
+        base_url = "http://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q16019&props=labels&format=json"
+        payload = {"action" : "wbgetentities",
+                   "ids": "Q" + str(self.wikidata_id),
+                   "props": "labels",
+                   "format": "json"
+                  }
+        response = requests.get(base_url, params=payload)
+        self.name = response.json()["entities"]["Q" + str(self.wikidata_id)]["labels"]["de"]["value"]
+
+    def __repr__(self):
+        return str(self.wikidata_id) + " " +  self.name
+
+
+def get_ministers():
+    base_url = "https://wdq.wmflabs.org/api"
+    query = "CLAIM[39:248352]{(CLAIM[580] AND CLAIM[582:4294967295]) OR (CLAIM[580] AND NOCLAIM[582])}"
+    payload = {"q": query,
+               "props": 18,
+              }
+    response = requests.get(base_url, params=payload)
+    json_response = response.json()
+    picture_file_names = json_response["props"]["18"]
+    filenames_by_id = dict((prop[0], prop[2]) for prop in picture_file_names)
+    all_ministers = []
+    for qid in json_response["items"]:
+        if qid in filenames_by_id.keys():
+            all_ministers.append(Minister(qid, filenames_by_id[qid]))
+        else:
+            all_ministers.append(Minister(qid, None))
+    print(all_ministers)
+    for minister in all_ministers:
+        minister.get_label()
+    print(all_ministers)
+
+if __name__ == "__main__":
+    get_ministers()
+
+
